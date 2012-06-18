340ca20cc7b9929b9a0e7926f87e0e6a1d3cd09c
Schlund <b.baerthlein@web.de>
2012-06-18 09:54:49 +0200

    Added own version tracking, Replaced RomManager with GooManager and added variables for it, removed Gapps from build, users have to install theirselves

--- products/common.mk
+++ products/common.mk
@@ -87,7 +87,8 @@ PRODUCT_COPY_FILES += \
 #    vendor/cyanogen/prebuilt/common/etc/init.d/10apps2sd:system/etc/init.d/10apps2sd
 
 PRODUCT_COPY_FILES +=  \
-    vendor/cyanogen/proprietary/RomManager.apk:system/app/RomManager.apk \
+	vendor/cyanogen/proprietary/GooManager.apk:system/app/GooManager.apk \
+#    vendor/cyanogen/proprietary/RomManager.apk:system/app/RomManager.apk \
 
 # Enable SIP+VoIP on all targets
 PRODUCT_COPY_FILES += \
@@ -97,8 +98,6 @@ PRODUCT_COPY_FILES += \
 #ADDITIONAL_DEFAULT_PROPERTIES += ro.secure=0
 
 ifdef CYANOGEN_WITH_GOOGLE
-
-ifdef FIX
 
     # use all present proprietary apk
     PRODUCT_COPY_FILES += $(shell test -f vendor/cyanogen/proprietary/*.apk && \
@@ -120,34 +119,6 @@ ifdef FIX
 	find vendor/cyanogen/proprietary -name '*.xml' \
 	-printf '%p:system/etc/permissions/%f ')
 
-else
-
-PRODUCT_COPY_FILES += \
-        vendor/cyanogen/proprietary/CarHomeGoogle.apk:./system/app/CarHomeGoogle.apk \
-        vendor/cyanogen/proprietary/GenieWidget.apk:./system/app/GenieWidget.apk \
-        vendor/cyanogen/proprietary/GoogleBackupTransport.apk:./system/app/GoogleBackupTransport.apk \
-        vendor/cyanogen/proprietary/GoogleCalendarSyncAdapter.apk:./system/app/GoogleCalendarSyncAdapter.apk \
-        vendor/cyanogen/proprietary/GoogleContactsSyncAdapter.apk:./system/app/GoogleContactsSyncAdapter.apk \
-        vendor/cyanogen/proprietary/GoogleFeedback.apk:./system/app/GoogleFeedback.apk \
-        vendor/cyanogen/proprietary/GooglePartnerSetup.apk:./system/app/GooglePartnerSetup.apk \
-        vendor/cyanogen/proprietary/GoogleQuickSearchBox.apk:./system/app/GoogleQuickSearchBox.apk \
-        vendor/cyanogen/proprietary/GoogleServicesFramework.apk:./system/app/GoogleServicesFramework.apk \
-        vendor/cyanogen/proprietary/FOTAKill.apk:./system/app/FOTAKill.apk \
-        vendor/cyanogen/proprietary/LatinImeTutorial.apk:./system/app/LatinImeTutorial.apk \
-        vendor/cyanogen/proprietary/MarketUpdater.apk:./system/app/MarketUpdater.apk \
-        vendor/cyanogen/proprietary/MediaUploader.apk:./system/app/MediaUploader.apk \
-        vendor/cyanogen/proprietary/NetworkLocation.apk:./system/app/NetworkLocation.apk \
-        vendor/cyanogen/proprietary/OneTimeInitializer.apk:./system/app/OneTimeInitializer.apk \
-        vendor/cyanogen/proprietary/SetupWizard.apk:./system/app/SetupWizard.apk \
-        vendor/cyanogen/proprietary/Talk.apk:./system/app/Talk.apk \
-        vendor/cyanogen/proprietary/Vending.apk:./system/app/Vending.apk \
-        vendor/cyanogen/proprietary/com.google.android.maps.xml:./system/etc/permissions/com.google.android.maps.xml \
-        vendor/cyanogen/proprietary/features.xml:./system/etc/permissions/features.xml \
-        vendor/cyanogen/proprietary/com.google.android.maps.jar:./system/framework/com.google.android.maps.jar \
-	vendor/cyanogen/proprietary/libvoicesearch.so:./system/lib/libvoicesearch.so
-
-endif 
-		
 else
     PRODUCT_PACKAGES += \
         Provision \
--- products/cyanogen_photon.mk
+++ products/cyanogen_photon.mk
@@ -13,19 +13,30 @@ $(call inherit-product, vendor/cyanogen/products/gsm.mk)
 PRODUCT_NAME := cyanogen_photon
 PRODUCT_BRAND := htc
 PRODUCT_DEVICE := photon
-PRODUCT_MODEL := Photon
+PRODUCT_MODEL := Photonic
 PRODUCT_MANUFACTURER := HTC
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=htc_photon BUILD_ID=GRK39F BUILD_FINGERPRINT=google/passion/passion:2.3.6/GRK39F/189904:user/release-keys PRIVATE_BUILD_DESC="passion-user 2.3.6 GRK39F 189904 release-keys"
 
 PRODUCT_SPECIFIC_DEFINES += TARGET_PRELINKER_MAP=$(TOP)/vendor/cyanogen/prelink-linux-arm-msm722x.map
 
-CYANOGEN_WITH_GOOGLE := true
+# CYANOGEN_WITH_GOOGLE := true
 
 # Broadcom FM radio
 $(call inherit-product, vendor/cyanogen/products/bcm_fm_radio.mk)
 
-# Release name and versioning
-PRODUCT_RELEASE_NAME := photonic
+# Release name and versioning, comment BUILDTYPE to create release
+CM_BUILD := photonic
+CM_BUILDTYPE := EXPERIMENTAL
+BUILD_NUM := 10
+CYANOGEN_EXTRAVERSION := r$(BUILD_NUM)
+
+# GOO Manager
+PRODUCT_PROPERTY_OVERRIDES += \
+	ro.goo.developerid=schlund \
+	ro.goo.board=$(PRODUCT_DEVICE) \
+	ro.goo.rom=$(PRODUCT_MODEL) \
+	ro.goo.version=$(BUILD_NUM)
+
 PRODUCT_VERSION_DEVICE_SPECIFIC :=
 -include vendor/cyanogen/products/common_versions.mk
 
